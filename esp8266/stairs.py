4743ff16134ec50d7b71e9c52e948de608c77824
Jim Mussared <jim.mussared@gmail.com>
2019-01-26 21:29:15 +1100

    Make night mode dimmer and specialcase birghtness=1 to turn off top section

--- esp8266/stairs.py
+++ esp8266/stairs.py
@@ -2,7 +2,11 @@ from machine import Timer, SPI
 from micropython import schedule
 from socket import getaddrinfo, socket
 
-_NUM_LEDS = const(170)
+_NUM_LEDS = const(162)
+
+_MODE_BLANK = const(0)
+_MODE_RAINBOW = const(1)
+_MODE_NIGHT = const(2)
 
 
 def rainbow(h, p):
@@ -33,8 +37,11 @@ def rainbow(h, p):
     p[1] = ((240-h) * 255) // 40
 
 
-mode = 2
+mode = _MODE_NIGHT
 brightness = 0b100
+
+next_mode = _MODE_NIGHT
+next_brightness = 0b100
 
 timer = Timer(-1)
 
@@ -60,6 +67,7 @@ def blank_frame(v, t):
   p0[1] = 0
   p0[2] = 0
   p0[3] = 0
+  return t < _NUM_LEDS
 
 def rainbow_frame(v, t):
   shift_frame(v)
@@ -67,40 +75,58 @@ def rainbow_frame(v, t):
   p0[0] = 0b11100000 | brightness
   rainbow(t % 240, p0)
   gamma(p0)
+  return True
 
 def night_frame(v, t):
+  if t >= 170:
+    return False
+  if brightness == 1 and (t < 25 or t % 2 == 0):
+    return blank_frame(v, t)
+
   shift_frame(v)
   p0 = v[0:4]
   p0[0] = 0b11100000 | brightness
-  p0[1] = 40
-  p0[2] = 120
-  p0[3] = 255
-  gamma(p0)
+  p0[1] = 0
+  p0[2] = 3
+  p0[3] = 32
+  return True
 
 def on_frame(x):
-  m = mode
+  global mode, brightness
+
   strip = x[0]
   pdata = x[1]
   v = x[2]
   t = x[3]
-  if m == 0:
-    blank_frame(v, t)
-  elif m == 1:
-    rainbow_frame(v, t)
-  elif m == 2:
-    night_frame(v, t)
 
-  strip.write(pdata)
-  x[3] = t + 1
+  if mode != next_mode or brightness != next_brightness:
+    t = 0
+    mode = next_mode
+    brightness = next_brightness
+
+  anim = True
+
+  if mode == _MODE_BLANK:
+    anim = blank_frame(v, t)
+  elif mode == _MODE_RAINBOW:
+    anim = rainbow_frame(v, t)
+  elif mode == _MODE_NIGHT:
+    anim = night_frame(v, t)
+
+  if anim:
+    strip.write(pdata)
+    x[3] = (t + 1) % 24000
+
   timer.init(mode=Timer.ONE_SHOT, period=1, callback=lambda t: schedule(on_frame, x))
 
 
 def server():
+  global next_mode, next_brightness
   strip = SPI(1, baudrate=2000000, polarity=0, phase=1)
-  pdata = bytearray([0x00] * 4 + [0x00] * 4 * _NUM_LEDS + [0xff] * 4)
+  pdata = bytearray([0x00] * 4 + [0x00] * 4 * _NUM_LEDS + [0xff] * 16)
   v = memoryview(pdata)[4:-4]
-  on_frame([strip, pdata, v, 0,])
-  
+  on_frame([strip, pdata, v, 0])
+
   addr = getaddrinfo('0.0.0.0', 80)[0][-1]
 
   s = socket()
@@ -116,12 +142,10 @@ def server():
         if not line or line == '\r\n':
             break
         if line.startswith('POST /mode/'):
-          global mode
-          mode = int(line.split('/')[2])
+          next_mode = int(line.split('/')[2])
           response = True
         if line.startswith('POST /brightness/'):
-          global brightness
-          brightness = int(line.split('/')[2])
+          next_brightness = int(line.split('/')[2])
           response = True
     if response:
       cl.send('200 OK')
