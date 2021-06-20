2676fb802879449b2aab5b510696ded0a92d08f9
Jim Mussared <jim.mussared@gmail.com>
2021-06-20 16:57:18 +1000

    messing around with dumb effects

--- esp8266/boot.py
+++ esp8266/boot.py
@@ -5,15 +5,16 @@ import gc
 
 def connect_sta():
     import network
+    import config
     ap_if = network.WLAN(network.AP_IF)
     ap_if.active(False)
     sta_if = network.WLAN(network.STA_IF)
-    if not sta_if.isconnected():
-        print('Connecting to network...')
-        sta_if.active(True)
-        sta_if.connect('Rainbows', '0417913600')
-        while not sta_if.isconnected():
-            pass
+    sta_if.active(True)
+    sta_if.config(dhcp_hostname='stairs')
+    print('Connecting to network...')
+    sta_if.connect(config.WIFI_SSID, config.WIFI_PASSWORD)
+    while not sta_if.isconnected():
+        pass
     print('Network config:', sta_if.ifconfig())
 
 connect_sta()
--- esp8266/stairs.py
+++ esp8266/stairs.py
@@ -1,13 +1,12 @@
 from machine import Timer, SPI
 from micropython import schedule
 from socket import getaddrinfo, socket
+from network import WLAN, STA_IF
+from random import getrandbits
+
+import math
 
 _NUM_LEDS = const(162)
-
-_MODE_BLANK = const(0)
-_MODE_RAINBOW = const(1)
-_MODE_NIGHT = const(2)
-
 
 def rainbow(h, p):
   # h is hue between 0-239.
@@ -36,14 +35,25 @@ def rainbow(h, p):
     p[2] = 0
     p[1] = ((240-h) * 255) // 40
 
+def purple(h, p):
+  t = 2*math.pi*h/24000
+  p[3] = int(100 + 40*math.sin(t * 100))
+  p[2] = 0
+  p[1] = int(150 + 30*math.sin(t * 20))
 
-mode = _MODE_NIGHT
-brightness = 0b100
+def green(h, p):
+  t = 2*math.pi*h/24000
+  p[3] = int(15 + 15*math.sin(t * 100))
+  p[2] = int(150 + 80*math.sin(t * 100) + 10*math.sin(t * 400))
+  p[1] = int(15 + 15*math.sin(t * 240))
 
-next_mode = _MODE_NIGHT
-next_brightness = 0b100
-
-timer = Timer(-1)
+def bloop(p):
+  if getrandbits(6) == 0:
+    rainbow(getrandbits(10) % 240, p)
+  else:
+    p[1] = 0
+    p[2] = 0
+    p[3] = 0
 
 @micropython.native
 def gamma(p):
@@ -73,7 +83,31 @@ def rainbow_frame(v, t):
   shift_frame(v)
   p0 = v[0:4]
   p0[0] = 0b11100000 | brightness
-  rainbow(t % 240, p0)
+  rainbow((t * 4) % 240, p0)
+  gamma(p0)
+  return True
+
+def purple_frame(v, t):
+  shift_frame(v)
+  p0 = v[0:4]
+  p0[0] = 0b11100000 | brightness
+  purple(t, p0)
+  gamma(p0)
+  return True
+
+def green_frame(v, t):
+  shift_frame(v)
+  p0 = v[0:4]
+  p0[0] = 0b11100000 | brightness
+  green(t, p0)
+  gamma(p0)
+  return True
+
+def bloop_frame(v, t):
+  shift_frame(v)
+  p0 = v[0:4]
+  p0[0] = 0b11100000 | brightness
+  bloop(p0)
   gamma(p0)
   return True
 
@@ -91,8 +125,21 @@ def night_frame(v, t):
   p0[3] = 32
   return True
 
-def on_frame(x):
+stop = False
+
+mode = purple_frame
+brightness = 0b100
+
+next_mode = mode
+next_brightness = 0b100
+
+timer = Timer(-1)
+
+data = None
+
+def on_frame(t):
   global mode, brightness
+  x = data
 
   strip = x[0]
   pdata = x[1]
@@ -104,51 +151,62 @@ def on_frame(x):
     mode = next_mode
     brightness = next_brightness
 
-  anim = True
-
-  if mode == _MODE_BLANK:
-    anim = blank_frame(v, t)
-  elif mode == _MODE_RAINBOW:
-    anim = rainbow_frame(v, t)
-  elif mode == _MODE_NIGHT:
-    anim = night_frame(v, t)
-
-  if anim:
+  if mode(v, t):
     strip.write(pdata)
     x[3] = (t + 1) % 24000
 
-  timer.init(mode=Timer.ONE_SHOT, period=1, callback=lambda t: schedule(on_frame, x))
+  if not stop:
+    timer.init(mode=Timer.ONE_SHOT, period=1, callback=on_frame)
 
+_MODES = [blank_frame, night_frame, rainbow_frame, purple_frame, green_frame, bloop_frame]
+
+def mode_index():
+  for i in range(len(_MODES)):
+    if _MODES[i] == mode:
+      return i
+  return 0
 
 def server():
-  global next_mode, next_brightness
+  global next_mode, next_brightness, stop, data
   strip = SPI(1, baudrate=2000000, polarity=0, phase=1)
   pdata = bytearray([0x00] * 4 + [0x00] * 4 * _NUM_LEDS + [0xff] * 16)
   v = memoryview(pdata)[4:-4]
-  on_frame([strip, pdata, v, 0])
+  data = [strip, pdata, v, 0]
+  on_frame(None)
 
   addr = getaddrinfo('0.0.0.0', 80)[0][-1]
+
+  print('Running server on', WLAN(STA_IF).ifconfig())
 
   s = socket()
   s.bind(addr)
   s.listen(1)
 
-  while True:
-    cl, addr = s.accept()
-    cl_file = cl.makefile('rwb', 0)
-    response = False
+  try:
+    stop = False
     while True:
-        line = cl_file.readline().decode()
-        if not line or line == '\r\n':
-            break
-        if line.startswith('POST /mode/'):
-          next_mode = int(line.split('/')[2])
-          response = True
-        if line.startswith('POST /brightness/'):
-          next_brightness = int(line.split('/')[2])
-          response = True
-    if response:
-      cl.send('200 OK')
-    else:
-      cl.send('500 Server Error')
-    cl.close()
+      cl, addr = s.accept()
+      cl_file = cl.makefile('rwb', 0)
+      response = False
+      while True:
+          line = cl_file.readline().decode()
+          if not line or line == '\r\n':
+              break
+          if line.startswith('POST'):
+            print(line.strip())
+          if line.startswith('POST /mode/'):
+            next_mode = globals()[line.split('/')[2] + '_frame']
+            response = True
+          elif line.startswith('POST /next/'):
+            next_mode = _MODES[(mode_index() + 1) % len(_MODES)]
+            response = True
+          elif line.startswith('POST /brightness/'):
+            next_brightness = int(line.split('/')[2])
+            response = True
+      if response:
+        cl.send('200 OK')
+      else:
+        cl.send('500 Server Error')
+      cl.close()
+  except:
+    stop = True
